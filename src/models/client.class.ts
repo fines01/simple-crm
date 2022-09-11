de6102ef7b0e1a400d5c1fcf0b386b7d7f430a30
I. Fritsch <ines.fritsch@gmail.com>
2022-09-11 12:42:40 +0200

    add client model

---
+++ src/models/client.class.ts
@@ -0,0 +1,36 @@
+export class Client {
+    firstName!: string;
+    lastName!: string;
+    street!: string;
+    zipCode!: number;
+    city!: string;
+    email!: string;
+    phone!: number;
+    // HouseNo / No. ev extra?
+    // country!: string; Tel.No. number
+    // address & also: billing address
+    // more Infos:  Contracts, Orders, Invoices & invoice status...
+
+    constructor(obj?: any) { // obj? --> ist optional
+        this.firstName = obj ? obj.firstName : '';
+        this.lastName = obj ? obj.lastName : '';
+        this.street = obj ? obj.street : '';
+        this.zipCode = obj ? obj.zipCode : '';
+        this.city = obj ? obj.city : '';
+        this.email = obj ? obj.email : '';
+        this.phone = obj ? obj.phone : '';
+    }
+
+    public toJSON() { // public s.d. in Komponenten u and Dateien darauf zugegriffen werden kann
+        return {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            street: this.street,
+            zipCode: this.zipCode,
+            city: this.city,
+            email: this.email,
+            phone: this.phone;
+        }
+    }
+
+}
